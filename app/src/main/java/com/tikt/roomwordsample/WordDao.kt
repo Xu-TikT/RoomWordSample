a447a7c665489d6466b4e6e6ec5899445b619362
skysafari <skysafari@foxmail.com>
2020-09-18 10:37:43 +0800

    增加注释

--- app/src/main/java/com/tikt/roomwordsample/WordDao.kt
+++ app/src/main/java/com/tikt/roomwordsample/WordDao.kt
@@ -11,7 +11,7 @@ interface WordDao {
     fun getAlphabetizedWords():LiveData<List<Word>>//以livedata返回
 
 
-    @Insert(onConflict = OnConflictStrategy.IGNORE)//如果新插入的单词已经有了则忽略
+    @Insert(onConflict = OnConflictStrategy.IGNORE)//如果新插入的单词和已存在的完全相同（包括主键id）则忽略,但是后来主键改成了自动增加的id,所以重复的单词仍可以添加
     suspend fun insert(word:Word)
 
     @Query("DELETE FROM word_table")
